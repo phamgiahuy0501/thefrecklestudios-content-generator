a25a233d9ed58fa35324509dae793f3f14a486e0
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-06-19 01:25:10 +0700

    Update: Complete generate in UI

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 import { generate } from "./src/generator.js"
+import { STYLE_CLASS_STRING } from "./src/utils/string.js"
 
-generate();
-// $("body").append(generate());
+$(".generate-button").on("click", function () {
+    $(`.generated-container .${STYLE_CLASS_STRING.WRAP_STYLE}`).remove()
 
+    $(".generated-container").append(generate())
+});
 
-$(".input-container ").on("focus", "input", function() {
-    // console.log($(this).parent().prop("className"));
+$(".input-container ").on("focus", "input", function () {
     if ($(this).parent().hasClass("inner")) {
         if ($(this).next().prop("tagName") == undefined) {
             $(this).after("<input type=\"text\" placeholder=\"Input content...\">")
         }
     } else {
         if ($(this).parent().next().prop("tagName") == undefined) {
-            $(this).parent().after("<div class=\"form-container\"><input type=\"text\" placeholder=\"Input content...\"><div class=\"form-container inner\"><input type=\"text\" placeholder=\"Input content...\"></div></div>")
+            $(this).parent().after("<div class=\"form-container outer\"><input type=\"text\" placeholder=\"Input content...\"><div class=\"form-container inner\"><input type=\"text\" placeholder=\"Input content...\"></div></div>")
         }
     }
 });
 
-// $(".input-container ").on("focusout", "input", function() {
-//     if ($(this).val() == "") {
-//         $(this).next().remove();
-//     }
-// });
+$(".input-container").on("focusout", "input", function () {
+    if ($(this).parent().hasClass("inner")) {
+        if ($(this).val() == "") {
+            $(this).next().remove();
+        }
+    } else {
+        if ($(this).val() == "") {
+            $(this).parent().next().remove();
+        }
+    }
+});
 
 // Animation
-$(".wrap-customer-style .dropdown-controller").on("click", function () {
+$(".generated-container").on("click", ".wrap-customer-style .dropdown-controller", function () {
     let plusObject = $(this).find(".plus")
     let minusObject = $(this).find(".minus")
     let content = $(this).next()
--- src/generator.js
+++ src/generator.js
@@ -1,5 +1,35 @@
 import * as Comp from "./generate-comp/components.js"
 
 export function generate() {
-    
+    const genElementList = [];
+    const outerElementList = $(".outer");
+
+    outerElementList.each(function () {
+        const inputList = $(this).find("input");
+        const inputListCount = inputList.length;
+        const genInnerElementList = [];
+
+        inputList.each(function (index, element) {
+            const textString = $(element).val();
+            if (textString == "") {
+                return;
+            }
+            if (index == 0) {
+                genElementList.push(Comp.titleDropdownComp(textString));
+                return;
+            }
+            if (index == 1 && inputListCount == 3) {
+                genElementList.push(Comp.contentAnimComp(textString));
+                return;
+            }
+            if (index == inputListCount - 2) {
+                genInnerElementList.push(Comp.innerContentItemComp(textString));
+                genElementList.push(Comp.contentAnimComp(genInnerElementList))
+                return
+            }
+            genInnerElementList.push(Comp.innerContentItemComp(textString));
+        });
+
+    });
+    return Comp.globalWrapComp(genElementList);
 }
