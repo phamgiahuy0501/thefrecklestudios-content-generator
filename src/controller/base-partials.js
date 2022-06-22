822a95129d68fa09e045bb27253e60d01acf6202
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-06-22 19:11:24 +0700

    Update: Fix redirect git index.htm

--- src/controller/base-partials.js
+++ src/controller/base-partials.js
@@ -1,15 +1,2 @@
-fetch("/views/partials/_header.htm")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        $("header").append(data);
-    });
-
-fetch("/views/partials/_footer.htm")
-    .then(response => {
-        return response.text()
-    })
-    .then(data => {
-        $("footer").append(data);
-    });
+$("header").load("../../views/partials/_header.htm")
+$("footer").load("../../views/partials/_footer.htm")
