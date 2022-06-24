bd904c053fdfce2cfb0a1fe6cc4c00f693450359
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-06-25 05:11:27 +0700

    Fix defect head hmtl import do not need to wait for the file ready

--- src/controller/base-partials.js
+++ src/controller/base-partials.js
@@ -1,5 +1,5 @@
+$("head").load("../../views/partials/head.htm");
 $(function () {
-    $("head").load("../../views/partials/head.htm");
     $("header").load("../../views/partials/header.htm");
     $("footer").load("../../views/partials/footer.htm");
 })
